753d10ed1b2c177598264fdbfbf6a72b2393bf7d
Jorge L. Fatta <jorge.fatta@southworks.com>
2016-05-06 16:06:37 -0300

    lesson 5 done

--- App.js
+++ App.js
@@ -1,22 +1,30 @@
 import React from 'react';
-import ReactDom from 'react-dom';
 class App extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            txt: 'this is the state txt',
+            cat: 0
+        }
+    }
+
+    update(e) {
+        this.setState({ txt: e.target.value })
+    }
+
+
     render() {
-        let txt = this.props.txt;
-        return <h1>{txt}</h1>
+        return (
+            <div>
+                <input type="text"
+                    onChange={this.update.bind(this) } />
+
+                <h1>{this.state.txt}</h1>
+            </div>
+        );
     }
 }
+export default App
 
-App.propTypes = {
-    txt: React.PropTypes.string,
-    cat: React.PropTypes.number.isRequired   
-}
 
-App.defaultProps = {
-    txt: 'this is the deafult txt'
-}
-
-ReactDom.render(
-    <App cat={5} />,
-    document.getElementById('app')
-);
