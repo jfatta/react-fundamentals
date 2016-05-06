60442ba6d1abce254143398d51778240f0582c54
Jorge L. Fatta <jorge.fatta@southworks.com>
2016-05-06 15:44:20 -0300

    lesson 3 done

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 class App extends React.Component {
     render() {
-        return <div>Hello World</div>
+        return (
+            <div>
+                <div>Hello World</div>
+                <b>Bold</b>
+            </div>
+        );
     }
 }
 
